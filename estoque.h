e5e713b3e71ba48e5c2ea61ce71a799eb77e09c6
Mallkarion <chrystianfrdz@gmail.com>
2026-09-20 17:33:55 -0400

    Update estoque.h aluno-4

--- estoque.h
+++ estoque.h
@@ -1,12 +1,10 @@
 #ifndef ESTOQUE_H
 #define ESTOQUE_H
 
-#define MAX_ITENS 10
-#define TAXA_PADRAO 0.05
-#define TAXA_DESCONTO 0.05
 #define MAX_ITENS 50
 #define ESTOQUE_MINIMO 5
 #define TAXA_PADRAO 0.10
+#define TAXA_DESCONTO 0.05
 #define TAXA_JUROS 0.08
 
 typedef struct {
@@ -24,6 +22,4 @@ float calcular_total(Produto lista[], int total);
 
 float aplicar_juros(float total);
 float aplicar_desconto(float total);
-#endif
-
 #endif
