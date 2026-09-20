bc8d1e6da6b7b6f692a91a2e63645694ea8dcf26
Mallkarion <chrystianfrdz@gmail.com>
2026-09-20 17:25:30 -0400

    Update main.c aluno-4

--- main.c
+++ main.c
@@ -6,6 +6,8 @@ void exibir_menu(void) {
     printf("\n=== CONTROLE DE ESTOQUE ===\n");
     printf("1 - Listar produtos\n");
     printf("2 - Exibir valor total em estoque\n");
+    printf("3 - Exibir total com desconto a vista\n");
+    printf("4 - Exibir total a prazo (com juros)\n");
     printf("0 - Sair\n");
     printf("Escolha uma opcao: ");
 }
@@ -55,6 +57,12 @@ int main(void) {
             case 2:
                 printf("\nTotal em estoque: R$ %.2f\n", calcular_total(estoque, total_produtos));
                 break;
+            case 3:
+                printf("\nTotal a vista: R$ %.2f\n", aplicar_desconto(calcular_total(estoque, total_produtos)));
+                break;
+            case 4:
+                printf("\nTotal a prazo: R$ %.2f\n", aplicar_juros(calcular_total(estoque, total_produtos)));
+                break;
             case 0:
                 printf("\nEncerrando o programa...\n");
                 break;
@@ -65,4 +73,11 @@ int main(void) {
     }
 
     return 0;
+}
+float aplicar_juros(float total) {
+    return total + total * TAXA_JUROS;
+}
+
+float aplicar_desconto(float total) {
+    return total - total * TAXA_DESCONTO;
 }
